dc27254c79ba9427f0ee10c7cd515ab01b5b3345
Amish Verma <amishverma1998@gmail.com>
2024-07-26 12:35:29 +1200

    Added the conditions to check for data

--- Ass-1-1.R
+++ Ass-1-1.R
@@ -1,3 +1,13 @@
-cat("hi")
+
 
 # My first comments
+
+# my function-1
+shapiro_wilk_test<-function(data,qqplot=FALSE){
+  if(!is.numeric(data)) stop("data must be numeric") # check data must be numeric
+  if(any(is.na(data))) stop("Data contains NA VALUES") # check data must not have na values
+  if(any(is.infinite(data))) stop("Data contains infinite values") # data doesnt not have inifnite values
+  if(length(data)<3) stop("Data must contain atlest 3 values") # atleast 3 values
+  
+    
+}
